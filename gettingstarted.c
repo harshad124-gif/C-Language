46d0c240f264bfb62257daf5782b681d22b4a676
harshad124-gif <harshadkale124@gmail.com>
2026-06-25 21:37:09 +0530

    let us c books 1st code

---
+++ gettingstarted.c
@@ -0,0 +1,31 @@
+/* Author : Gekay Date : 25/06/2023*/
+
+#include <stdio.h>
+
+int main()
+
+{int
+
+int p,n;
+
+float r,si;
+
+p=1000;
+
+n=3;
+
+r=8.5
+
+
+
+/* Formula for simple interest */
+
+si=p*n*r/100;
+
+printf ("%f/n",si)
+
+return 0;
+
+} 
+
+
